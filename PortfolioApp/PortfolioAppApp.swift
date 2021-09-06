0c11daa5b4bebeff850fed88ec9480423fb6ea29
rushi-pa <rushipatel102001@gmail.com>
2021-09-06 13:29:56 -0400

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Rushi Patel on 2021-09-06.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
